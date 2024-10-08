317a171d23f50ed14f4392de4648e55cf2f11c0c
Nischal Karki <nischal.karki.nkk@gmail.com>
2024-10-07 23:51:18 -0400

    Compiled everything into functions for a new jupyter notebook. Added helpful keyword arguments for increased flexibility.

--- leaf-example-tutorial/dataloaders.py
+++ leaf-example-tutorial/dataloaders.py
@@ -38,6 +38,11 @@ class NPYDataset(Dataset):
         #x = x.unsqueeze(0)
         return x, y
     
+    def get_label(self, index):
+        if type(index) == torch.Tensor:
+            index = index.numpy()
+        return self.classes[index]
+    
     def __len__(self):
         return len(self.data)
     
@@ -49,27 +54,47 @@ def rescale(arr):
     return new_arr
     
 # function to create the datasets
-def create_datasets():
+def create_datasets(data=os.getcwd()+'/example_data/ect_output/', valid_split=VALID_SPLIT, log_level='INFO'):
     """
     Function to load in images (ect or outline) to dataset
     """
+    numpy_data=[]
+    numpy_target=[]
 
-    image_path = os.getcwd()+'/example_data/ect_output/'
-    print(image_path)
+    log_level = log_level == True or str(log_level).upper() == 'INFO'
+    
+    if type(data) == dict:
+        classes = list(data.keys())
+        for i, category in enumerate(data):
+            for img in data[category]:
+                numpy_data.append( img )
+                numpy_target.append( i )
+        numpy_data = np.float32(numpy_data)
+        numpy_target = np.float32(numpy_target)
+        train, valid_data, y_train, y_valid = train_test_split(numpy_data, numpy_target, test_size=valid_split, shuffle=True)
+        if log_level:
+            print('ECT data; using only normalize, rotation transforms on training data')
+        train_data = NPYDataset(train, y_train, classes, transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize(0.5,0.5), ROTATE_TRANSLATE()]))   
+        valid_data = NPYDataset(valid_data, y_valid, classes, transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize(0.5,0.5), ROTATE_TRANSLATE()]))
+        return train_data, valid_data
+    else:
+        image_path = data
+
+    # image_path = os.getcwd()+'/example_data/ect_output/'
+    
     # Dict to map class names to indices
     classes = []
     for (dirpath, dirnames, filenames) in os.walk(image_path):
-        print(dirnames)
+        if log_level:
+            print(dirnames)
         classes.extend(dirnames)
         break
     idx_to_class = {i:j for i, j in enumerate(classes)}
     class_to_idx = {value:key for key,value in idx_to_class.items()}
     num_classes = len(classes)
-    print('num_classes=',num_classes)
+    if log_level:
+        print('num_classes=',num_classes)
 
-
-    numpy_data=[]
-    numpy_target=[]
     classes_count=[0 for c in classes]
     for path, subdirs, files in os.walk(image_path):
         
@@ -90,12 +115,13 @@ def create_datasets():
                 classes_count[class_to_idx[label]]+=1
                 
             
-    numpy_data = np.array(numpy_data)
-    numpy_target = np.array(numpy_target)
+    numpy_data = np.float32(numpy_data)
+    numpy_target = np.float32(numpy_target)
    
-    train, valid_data, y_train, y_valid = train_test_split(numpy_data, numpy_target, test_size=VALID_SPLIT, shuffle=True)
+    train, valid_data, y_train, y_valid = train_test_split(numpy_data, numpy_target, test_size=valid_split, shuffle=True)
 
-    print('ECT data; using only normalize, rotation transforms on training data')
+    if log_level:
+        print('ECT data; using only normalize, rotation transforms on training data')
     train_data = NPYDataset(train, y_train, classes, transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize(0.5,0.5), ROTATE_TRANSLATE()]))   
     valid_data = NPYDataset(valid_data, y_valid, classes, transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize(0.5,0.5), ROTATE_TRANSLATE()]))
 
@@ -107,7 +133,7 @@ def create_datasets():
 
 
 
-def create_data_loaders(train_data, valid_data):
+def create_data_loaders(train_data, valid_data, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS):
     """
     Function to build the data loaders.
     Parameters:
@@ -115,9 +141,9 @@ def create_data_loaders(train_data, valid_data):
     :param valid_data: The validation dataset.
     """
 
-    train_loader = DataLoader(dataset = train_data, batch_size = BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
+    train_loader = DataLoader(dataset = train_data, batch_size = batch_size, shuffle=True, num_workers=num_workers)
 
-    valid_loader = DataLoader(dataset = valid_data, batch_size = BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)    
+    valid_loader = DataLoader(dataset = valid_data, batch_size = batch_size, shuffle=True, num_workers=num_workers)    
 
     return train_loader, valid_loader
 
